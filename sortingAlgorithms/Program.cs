644a03c0b981baa0121def13add349a0e86d3327
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2018-04-12 21:37:03 +0300

    Test1
    Testing insertion sort and selection sort&

--- sortingAlgorithms/Program.cs
+++ sortingAlgorithms/Program.cs
@@ -40,9 +40,33 @@ namespace sortingAlgorithms
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(">Testing insertion sort");
+            Console.WriteLine("Unordered array:");
             int[] arr = new int[] { -12, 5, 2, 12, 6, 1, 1, 0, 0, 7, 18 };
             Algorithm.insertionSort(arr);
             foreach (var elem in arr) {
+                Console.Write($"{elem} ");
+            }
+            Console.WriteLine("\nOrdered array:");
+            int[] arr1 = new int[] { -5, -2, 0, 4, 7, 20, 43, 98 };
+            Algorithm.insertionSort(arr1);
+            foreach (var elem in arr1) {
+                Console.Write($"{elem} ");
+            }
+            Console.WriteLine('\n');
+
+            Console.WriteLine(">Testing selection sort");
+            Console.WriteLine("Unordered array:");
+            int[] arr2 = new int[] { -100, 50, 65, 1, 0, 1, 1, 1, 90, 54, 34, -4, 0, 23, -11 };
+            Algorithm.selectionSort(arr2, arr2.Length);
+            foreach (var elem in arr2) {
+                Console.Write($"{elem} ");
+            }
+            Console.WriteLine("\nOrdered array:");
+            int[] arr3 = new int[] { -98, -4, 0, 1, 1, 1, 9, 34, 67, 87, 87 };
+            Algorithm.selectionSort(arr3, arr3.Length);
+            foreach (var elem in arr3)
+            {
                 Console.Write($"{elem} ");
             }
         }
