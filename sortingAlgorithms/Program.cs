baa2be49818cf3f51147c333cf3f0e5ce53e34a6
DanilaMonets <38329448+DanilaMonets@users.noreply.github.com>
2018-04-12 21:30:52 +0300

    Quicksort algorithm added

--- sortingAlgorithms/Program.cs
+++ sortingAlgorithms/Program.cs
@@ -99,6 +99,35 @@ namespace sortingAlgorithms
                 }
             }
         }
+        public static void quickSort (int[] arr, int left, int right) 
+        {
+            int i = left, j = right;
+            int curr;
+            int pivot = arr[(left + right) / 2];
+
+            while (i <= j) {
+                while (arr[i] < pivot) {
+                    i++;
+                }
+                while (arr[j] > pivot) {
+                    j--;
+                }
+                if (i <= j) {
+                    curr = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = curr;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j) {
+                quickSort(arr, left, j);
+            }
+            if (i < right) {
+                quickSort(arr, i, right);
+            }
+        }
     }
     class MainClass
     {
